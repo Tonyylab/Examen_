fff1f7c0ee91ab3db272c4594e0d5c7d3cdafc04
Framaximo <framaximo4@gmail.com>
2024-12-09 14:31:30 -0300

    AAAAAAAAAAAH
    -Reemplazar Crosshair
    -Añadir descripciones a los ITEMDATA (ubicados en script)
    -Añadir detalle a Menu Principal
    -Reemplazar png victoria
    
    -Error al craftear (no se actualiza adecuadamente, por lo que puedes craftear varias veces ciertos items)
    -Problema en la anim de atauqe (ciertas armas se ven raras, anim hecha para el martillo solamente)
    -no hay interaccion para el bowl de agua (me dio paja)
    -no se puede dropear bowl de fruta (no habia prefab)
    -las armas no tienen rigidbody al dropearlas (ups)

--- Examen_/Assets/Scripts/PlayerController.cs
+++ Examen_/Assets/Scripts/PlayerController.cs
@@ -102,26 +102,26 @@ public class PlayerController : MonoBehaviour
     {
         if (context.phase == InputActionPhase.Started)
         {
-            if (isGrounded())
+            if (isGrounded() == true)
             {
                 rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             }
         }
     }
 
-    bool isGrounded()
+     bool isGrounded()
     {
         Ray[] rays = new Ray[4]
         {
-            new Ray (transform.position + (transform.forward * 0.2f)+(Vector3.up*0.01f),Vector3.down),
-            new Ray(transform.position + (-transform.forward * 0.2f)+(Vector3.up*0.01f),Vector3.down),
-            new Ray(transform.position + (transform.right * 0.2f)+(Vector3.up*0.01f),Vector3.down),
-            new Ray(transform.position + (-transform.right * 0.2f)+(Vector3.up*0.01f),Vector3.down)
+            new Ray (transform.position + (transform.forward * 0.2f)+(Vector3.up*0.01f),Vector3.down*8),
+            new Ray(transform.position + (-transform.forward * 0.2f)+(Vector3.up*0.01f),Vector3.down*8),
+            new Ray(transform.position + (transform.right * 0.2f)+(Vector3.up*0.01f),Vector3.down * 8),
+            new Ray(transform.position + (-transform.right * 0.2f)+(Vector3.up*0.01f),Vector3.down*8)
 
         };
         for (int i = 0; i < rays.Length; i++)
         {
-            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            if (Physics.Raycast(rays[i], 8, groundLayerMask))
             {
                 return true;
             }
@@ -132,10 +132,10 @@ public class PlayerController : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position + (transform.forward * 0.2f), Vector3.down);
-        Gizmos.DrawRay(transform.position + (-transform.forward * 0.2f), Vector3.down);
-        Gizmos.DrawRay(transform.position + (transform.right * 0.2f), Vector3.down);
-        Gizmos.DrawRay(transform.position + (-transform.right * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (transform.forward * 0.2f), Vector3.down*7);
+        Gizmos.DrawRay(transform.position + (-transform.forward * 0.2f), Vector3.down * 7);
+        Gizmos.DrawRay(transform.position + (transform.right * 0.2f), Vector3.down * 7);
+        Gizmos.DrawRay(transform.position + (-transform.right * 0.2f), Vector3.down * 7);
 
     }
 
